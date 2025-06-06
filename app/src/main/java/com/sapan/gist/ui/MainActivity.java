a5b1fc3bd70c61596a2320c569c68fd37e6b57f6
sapan <sapanravidas@gmail.com>
2025-06-06 19:26:35 +0530

    setup

--- app/src/main/java/com/sapan/gist/ui/MainActivity.java
+++ app/src/main/java/com/sapan/gist/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.sapan.gist;
+package com.sapan.gist.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.sapan.gist.R;
 
 public class MainActivity extends AppCompatActivity {
 
