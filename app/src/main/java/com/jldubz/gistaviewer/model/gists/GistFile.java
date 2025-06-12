2dd14a2dcb880dad61ab340a160f219723f72dd4
sapan <sapanravidas@gmail.com>
2025-06-12 10:09:35 +0530

    initial commit | complete POC is here

---
+++ app/src/main/java/com/jldubz/gistaviewer/model/gists/GistFile.java
@@ -0,0 +1,24 @@
+package com.jldubz.gistaviewer.model.gists;
+
+import androidx.annotation.Keep;
+
+@Keep
+public class GistFile {
+
+    /*
+    Modified sample data from: https://developer.github.com/v3/gists/#list-a-users-gists
+    {
+        "filename": "hello_world.rb"
+    }
+    */
+
+    private String filename;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+}
